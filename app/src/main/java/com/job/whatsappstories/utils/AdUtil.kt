2c39a2f81f1f0bdaa119d4cfd3c0a8b09e38d2b6
Job Getabu <getabujob@gmail.com>
2019-01-04 02:25:04 +0300

    added simple biz logic

---
+++ app/src/main/java/com/job/whatsappstories/utils/AdUtil.kt
@@ -0,0 +1,10 @@
+package com.job.whatsappstories.utils
+
+/**
+ * Created by Job on Friday : 1/4/2019.
+ */
+
+
+fun multipleOfTwo(num: Int) = num % 2 == 0
+
+fun multipleOfFive(num: Int) = num % 5 == 0
---
+++ app/src/main/java/com/job/whatsappstories/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.job.whatsappstories.utils;
+
+/**
+ * Created by Job on Friday : 1/4/2019.
+ */
+public class Constants {
+    public static final String IMAGE_SAVE_CLICKS = "IMAGE_SAVE_CLICKS";
+    public static final String VIDEO_SAVE_CLICKS = "VIDEO_SAVE_CLICKS";
+
+}

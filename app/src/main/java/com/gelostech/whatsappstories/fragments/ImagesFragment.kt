3cc0e4465c70cadc0e1126b48846139ebb449f9b
Job Getabu <getabujob@gmail.com>
2019-01-03 17:08:41 +0300

    init commit preDone work

---
+++ app/src/main/java/com/gelostech/whatsappstories/fragments/ImagesFragment.kt
@@ -0,0 +1,108 @@
+package com.gelostech.whatsappstories.fragments
+
+
+import android.os.Bundle
+import android.os.Handler
+import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.GridLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.gelostech.whatsappstories.R
+import com.gelostech.whatsappstories.models.Story
+import com.gelostech.whatsappstories.adapters.StoriesAdapter
+import com.gelostech.whatsappstories.callbacks.StoryCallback
+import com.gelostech.whatsappstories.commoners.BaseFragment
+import com.gelostech.whatsappstories.commoners.K
+import com.gelostech.whatsappstories.commoners.StoryOverview
+import com.gelostech.whatsappstories.utils.RecyclerFormatter
+import com.gelostech.whatsappstories.utils.hideView
+import com.gelostech.whatsappstories.utils.showView
+import kotlinx.android.synthetic.main.fragment_images.*
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.File
+
+
+class ImagesFragment : BaseFragment(), StoryCallback {
+    private lateinit var adapter: StoriesAdapter
+    private var refreshing = false
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_images, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        loadStories()
+    }
+
+    private fun initViews() {
+        rv.setHasFixedSize(true)
+        rv.layoutManager = GridLayoutManager(activity!!, 3)
+        rv.addItemDecoration(RecyclerFormatter.GridItemDecoration(activity!!, 3, 5))
+        rv.itemAnimator = DefaultItemAnimator()
+        (rv.itemAnimator as DefaultItemAnimator).supportsChangeAnimations = false
+
+        adapter = StoriesAdapter(this, activity!!)
+        rv.adapter = adapter
+
+    }
+
+    private fun loadStories() {
+        if (!storagePermissionGranted()) {
+            requestStoragePermission()
+            return
+        }
+
+        val dir = File(K.WHATSAPP_STORIES)
+        if (!dir.exists())
+            dir.mkdirs()
+
+        doAsync {
+            val files = dir.listFiles { _, s ->
+                s.endsWith(".png") || s.endsWith(".jpg") || s.endsWith(".jpeg") }
+
+            uiThread {
+
+                if (files.isNotEmpty()) {
+                    hasStories()
+                    if (refreshing) adapter.clearStories()
+
+                    for (file in files.sortedBy { it.lastModified() }.reversed()) {
+                        val story = Story(K.TYPE_IMAGE, file.absolutePath)
+                        adapter.addStory(story)
+                    }
+
+                    refreshing = false
+                } else {
+                    noStories()
+                }
+            }
+
+        }
+
+    }
+
+    private fun noStories() {
+        rv?.hideView()
+        empty?.showView()
+    }
+
+    private fun hasStories() {
+        empty?.hideView()
+        rv?.showView()
+    }
+
+    override fun onStoryClicked(v: View, story: Story) {
+        val overview = StoryOverview(activity!!, story)
+        overview.show()
+    }
+
+}

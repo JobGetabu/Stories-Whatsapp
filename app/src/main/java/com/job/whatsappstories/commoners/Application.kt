7a07b840b340259c0d1d55f067a76bfeeff553b4
Job Getabu <getabujob@gmail.com>
2019-01-04 18:19:34 +0300

    added crashlytics

--- app/src/main/java/com/job/whatsappstories/commoners/Application.kt
+++ app/src/main/java/com/job/whatsappstories/commoners/Application.kt
@@ -2,6 +2,7 @@ package com.job.whatsappstories.commoners
 
 import android.support.multidex.MultiDexApplication
 import com.google.android.gms.ads.MobileAds
+import com.job.whatsappstories.BuildConfig
 import com.job.whatsappstories.R
 import timber.log.Timber
 
@@ -14,5 +15,9 @@ class Application : MultiDexApplication() {
 
         //initializing AdMob account
         MobileAds.initialize(this, getString(R.string.admob_app_id))
+
+        if(!BuildConfig.DEBUG){
+
+        }
     }
 }

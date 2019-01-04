c088b7e3ee3f6f4e95435ec7569973be1e888d02
Job Getabu <getabujob@gmail.com>
2019-01-04 18:22:36 +0300

    added crash reporting and notification messaging

--- app/src/main/java/com/job/whatsappstories/commoners/Application.kt
+++ app/src/main/java/com/job/whatsappstories/commoners/Application.kt
@@ -1,9 +1,11 @@
 package com.job.whatsappstories.commoners
 
 import android.support.multidex.MultiDexApplication
+import com.crashlytics.android.Crashlytics
 import com.google.android.gms.ads.MobileAds
 import com.job.whatsappstories.BuildConfig
 import com.job.whatsappstories.R
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class Application : MultiDexApplication() {
@@ -17,7 +19,7 @@ class Application : MultiDexApplication() {
         MobileAds.initialize(this, getString(R.string.admob_app_id))
 
         if(!BuildConfig.DEBUG){
-
+            Fabric.with(this,Crashlytics.getInstance())
         }
     }
 }

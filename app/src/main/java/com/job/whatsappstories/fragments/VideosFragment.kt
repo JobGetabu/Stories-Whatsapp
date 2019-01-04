f5c08a740edf2b3e4a9d7d840d0a35f8d4e2660e
Job Getabu <getabujob@gmail.com>
2019-01-04 17:15:58 +0300

    added logic for video ads

--- app/src/main/java/com/job/whatsappstories/fragments/VideosFragment.kt
+++ app/src/main/java/com/job/whatsappstories/fragments/VideosFragment.kt
@@ -1,13 +1,18 @@
 package com.job.whatsappstories.fragments
 
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.GridLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.reward.RewardItem
+import com.google.android.gms.ads.reward.RewardedVideoAd
+import com.google.android.gms.ads.reward.RewardedVideoAdListener
 import com.job.whatsappstories.R
 import com.job.whatsappstories.adapters.StoriesAdapter
 import com.job.whatsappstories.callbacks.StoryCallback
@@ -15,17 +20,20 @@ import com.job.whatsappstories.commoners.BaseFragment
 import com.job.whatsappstories.commoners.K
 import com.job.whatsappstories.commoners.StoryOverview
 import com.job.whatsappstories.models.Story
-import com.job.whatsappstories.utils.RecyclerFormatter
-import com.job.whatsappstories.utils.hideView
-import com.job.whatsappstories.utils.showView
+import com.job.whatsappstories.utils.*
 import kotlinx.android.synthetic.main.fragment_videos.*
 import kotlinx.android.synthetic.main.video_empty.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.uiThread
+import timber.log.Timber
 import java.io.File
 
-class VideosFragment : BaseFragment(), StoryCallback {
+class VideosFragment : BaseFragment(), StoryCallback, RewardedVideoAdListener {
+
     private lateinit var adapter: StoriesAdapter
+    private lateinit var mRewardedVideoAd: RewardedVideoAd
+    private lateinit var sharedPrefs: SharedPreferences
+    private lateinit var sharedPrefsEditor: SharedPreferences.Editor
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -38,6 +46,14 @@ class VideosFragment : BaseFragment(), StoryCallback {
 
         initViews()
         loadStories()
+
+        sharedPrefs = activity!!.getSharedPreferences(activity?.applicationContext?.packageName, Context.MODE_PRIVATE)
+        sharedPrefsEditor = activity!!.getSharedPreferences(activity?.applicationContext?.packageName, Context.MODE_PRIVATE).edit()
+
+        mRewardedVideoAd = MobileAds.getRewardedVideoAdInstance(activity)
+        mRewardedVideoAd.rewardedVideoAdListener = this
+
+        initLoadVideoAdUnit(mRewardedVideoAd,activity!!)
     }
 
     private fun initViews() {
@@ -99,6 +115,66 @@ class VideosFragment : BaseFragment(), StoryCallback {
     override fun onStoryClicked(v: View, story: Story) {
         val overview = StoryOverview(activity!!, story)
         overview.show()
+
+        adBizLogicVideo(mRewardedVideoAd,story, sharedPrefsEditor,sharedPrefs)
+
+    }
+
+    //region VIDEO_REWARDED IMPLEMENTATION
+    override fun onRewarded(reward: RewardItem) {
+        // Reward the user. no reward
+
+    }
+
+    override fun onRewardedVideoAdLeftApplication() {
+        Timber.tag("AdUtil").d("onRewardedVideoAdLeftApplication")
+    }
+
+    override fun onRewardedVideoAdClosed() {
+        Timber.tag("AdUtil").d("onRewardedVideoAdClosed")
+        //good place to reload
+        initLoadVideoAdUnit(mRewardedVideoAd,activity!!)
+    }
+
+    override fun onRewardedVideoAdFailedToLoad(errorCode: Int) {
+        Timber.tag("AdUtil").d("onRewardedVideoAdFailedToLoad $errorCode")
+
+    }
+
+    override fun onRewardedVideoAdLoaded() {
+        Timber.tag("AdUtil").d("onRewardedVideoAdLoaded")
+    }
+
+    override fun onRewardedVideoAdOpened() {
+        Timber.tag("AdUtil").d("onRewardedVideoAdOpened")
+    }
+
+    override fun onRewardedVideoStarted() {
+        Timber.tag("AdUtil").d("onRewardedVideoStarted")
+    }
+
+    override fun onRewardedVideoCompleted() {
+        Timber.tag("AdUtil").d("onRewardedVideoCompleted")
+        //good place to reload
+        initLoadVideoAdUnit(mRewardedVideoAd,activity!!)
+    }
+
+
+    //endregion
+
+    override fun onPause() {
+        super.onPause()
+        mRewardedVideoAd.pause(activity)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mRewardedVideoAd.resume(activity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mRewardedVideoAd.destroy(activity)
     }
 
 }

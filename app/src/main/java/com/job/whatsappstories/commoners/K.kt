3a5d5fd756c90d607fcf44f6f92f7d66157f39d4
Job Getabu <getabujob@gmail.com>
2019-01-10 01:49:01 +0300

    added referral and business support

--- app/src/main/java/com/job/whatsappstories/commoners/K.kt
+++ app/src/main/java/com/job/whatsappstories/commoners/K.kt
@@ -10,7 +10,8 @@ object K {
     const val TYPE_VIDEO = 1
 
     var WHATSAPP_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/WhatsApp/Media/.Statuses"
+    var WHATSAPP_BUSINESS_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/WhatsApp Business/Media/.Statuses"
     var GBWHATSAPP_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/GBWhatsApp/Media/.Statuses"
-    var SAVED_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/WhatsApp Stories"
+    var SAVED_STORIES = Environment.getExternalStorageDirectory().absolutePath +"/Stories Saver"
 
 }

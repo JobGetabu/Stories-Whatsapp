2247ac497614c22234a83cd4c28157e5735868f5
Job Getabu <getabujob@gmail.com>
2019-01-13 14:13:09 +0300

    model user added

--- app/src/main/java/com/job/whatsappstories/models/User.kt
+++ app/src/main/java/com/job/whatsappstories/models/User.kt
@@ -3,4 +3,4 @@ package com.job.whatsappstories.models
 /**
  * Created by Job on Thursday : 1/10/2019.
  */
-data class User(val userid: String, val referredby: String, val refercount: Int)
+data class User(val userid: String, val referredby: String, val refercount: Int, val token: String, val ispro: Boolean)
